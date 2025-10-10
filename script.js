605a6a595419a16faa746cd919e22eae4ed86b99
Hoarau Erwan <ehoarau003@iutbayonne.univ-pau.fr>
2025-10-10 09:35:16 +0200

    modif trad lien contact

--- script.js
+++ script.js
@@ -56,6 +56,9 @@ const translations = {
         competencesTitle: "Mes Compétences",
 
         contact: "Contact",
+        contactLink1: "Vous pouvez regarder mon GitHub grâce à ce lien :",
+        contactLink2: "Vous pouvez regarder mon LinkedIn grâce à ce lien :",
+        contactLink3: "Ou me contacter directement via le formulaire ci-dessous :",
         inputName: "Votre nom",
         inputEmail: "Votre email",
         inputMsg: "Votre message",
@@ -92,6 +95,9 @@ const translations = {
         competencesTitle: "My Skills",
 
         contact: "Contact",
+        contactLink1: "You can check out my GitHub through this link:",
+        contactLink2: "You can check out my LinkedIn through this link:",
+        contactLink3: "Or contact me directly via the form below:",
         inputName: "Your name",
         inputEmail: "Your email",
         inputMsg: "Your message",
@@ -141,6 +147,9 @@ translateBtn.addEventListener("click", () => {
 
     // Section "Contact"
     document.querySelector("#contact h2").innerHTML = t.contact;
+    document.querySelectorAll(".contact-links a")[0].innerHTML = t.contactLink1;
+    document.querySelectorAll(".contact-links a")[1].innerHTML = t.contactLink2;
+    document.querySelectorAll(".contact-links p")[0].innerHTML = t.contactLink3;
     document.querySelector("#contact input[name='name']").placeholder = t.inputName;
     document.querySelector("#contact input[name='email']").placeholder = t.inputEmail;
     document.querySelector("#contact textarea").placeholder = t.inputMsg;
